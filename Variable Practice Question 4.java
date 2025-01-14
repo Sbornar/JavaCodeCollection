20b5c96971f03c47f14b5e9d86aac364071498a3
Sbornar <100412358+Sbornar@users.noreply.github.com>
2025-01-14 23:04:09 +0530

    Variable Practice Question 4.java

---
+++ Variable Practice Question 4.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    public static void main(String[] args) {
+           int a = 1042;
+           int b = 7;
+           int quo , rem;
+
+           quo = a / b;
+           rem  = a % b;
+
+           System.out.print(quo);
+           System.out.print(" ");
+           System.out.print(rem);
+    }
+}
