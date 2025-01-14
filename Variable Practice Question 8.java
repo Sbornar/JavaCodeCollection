201b5b03b9c28b4f20c1a4f309b3ddd6c0f1ce7e
Sbornar <100412358+Sbornar@users.noreply.github.com>
2025-01-14 23:05:55 +0530

    Create Variable Practice Question 8.java

---
+++ Variable Practice Question 8.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        
+       int input = 8;
+
+      
+
+       System.out.println("8 x 1 = " +input * 1);
+       System.out.println("8 x 2 = " +input * 2);
+       System.out.println("8 x 3 = " +input * 3);
+       System.out.println("8 x 4 = " +input * 4);
+       System.out.println("8 x 5 = " +input * 5);
+       System.out.println("8 x 6 = " +input * 6);
+       System.out.println("8 x 7 = " +input * 7);
+       System.out.println("8 x 8 = " +input * 8);
+       System.out.println("8 x 9 = " +input * 9);
+       System.out.println("8 x 10 = " +input * 10);
+
+
+    }
+}
