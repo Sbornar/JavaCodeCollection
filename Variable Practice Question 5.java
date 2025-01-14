58836751ab3cb7162a11a8a5f193ea59f880aad6
Sbornar <100412358+Sbornar@users.noreply.github.com>
2025-01-14 23:08:15 +0530

    Create Variable Practice Question 5.java

---
+++ Variable Practice Question 5.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		float v1 = 10.5f;
+		float v2 = 12.5f;
+		float v3 = 12.15f;
+
+		int average = (int)(v1 + v2 + v3)/3;
+
+		System.out.println(average);
+	}
+}
