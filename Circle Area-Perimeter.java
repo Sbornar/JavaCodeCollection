8a196a7495e36da21b6d131ddec79b0f11276ae0
Sbornar <100412358+Sbornar@users.noreply.github.com>
2025-01-15 10:51:28 +0530

    Circle Area-Perimeter.java

---
+++ Circle Area-Perimeter.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner scr = new Scanner(System.in);
+
+        long Radius = scr.nextLong();
+        int pI = 3;
+
+        System.out.println(pI * Radius *Radius);
+        System.out.println(2*pI*Radius);
+    }
+}
