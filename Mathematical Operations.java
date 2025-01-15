4f15f459d91ce1536e73ff229937afb65edd5481
Sbornar <100412358+Sbornar@users.noreply.github.com>
2025-01-15 10:49:05 +0530

    Create Mathematical Operations.java

---
+++ Mathematical Operations.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) throws Throwable {
+    
+      int A , B;
+
+      Scanner scn = new Scanner(System.in);
+
+      A = scn.nextInt();
+      B = scn.nextInt();
+     
+      
+        System.out.println(A+B);
+         System.out.println(A-B);
+          System.out.println(A*B);
+           System.out.println(A/B);
+            System.out.println(A%B);
+
+    
+
+
+
+  }
+}
