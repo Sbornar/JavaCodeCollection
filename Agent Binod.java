246b7cdb15a0a97c7742ca6acdbf7460c5d2f734
Sbornar <100412358+Sbornar@users.noreply.github.com>
2025-01-15 10:50:08 +0530

    Agent Binod.java

---
+++ Agent Binod.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+         
+         String name ;
+
+         Scanner scr = new Scanner(System.in);
+
+         name = scr.nextLine();
+
+         System.out.println("Hi my name is Agent "+name);
+    }
+}
