213cc9eb94cc2c119970b0628bad5021a486443b
Sbornar <100412358+Sbornar@users.noreply.github.com>
2025-01-16 23:00:55 +0530

    Celsius to Fahrenheit.java

---
+++ Celsius to Fahrenheit.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        int tempInCelsius ;
+
+        Scanner src = new Scanner(System.in);
+
+        tempInCelsius = src.nextInt();
+
+        System.out.println(tempInCelsius * 9/5+32);
+
+
+    }
+}
